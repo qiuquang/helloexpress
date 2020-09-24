606e695abe1742ac78918e87874470f0dc08f6ad
qiuquan <qiuquan@qdcares.cn>
2020-09-24 17:28:55 +0800

    中间件

--- hello/routes/index.js
+++ hello/routes/index.js
@@ -8,4 +8,37 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//world路由
+router.get('/world', function(req, res, next) {
+  // v0.1版  前面是模板文件名  后面是传入到页面中的对象
+  res.render('index', { title: 'Hello World!' });
+});
+
+router.post('/world', function(req, res, next) {
+  // v0.1版  前面是模板文件名  后面是传入到页面中的对象
+  res.render('index', { title: 'post Hello World!' });
+});
+
+router.put('/world', function(req, res, next) {
+  // v0.1版  前面是模板文件名  后面是传入到页面中的对象
+  res.render('index', { title: 'put Hello World!' });
+});
+
+router.delete('/world', function(req, res, next) {
+  // v0.1版  前面是模板文件名  后面是传入到页面中的对象
+  res.render('index', { title: 'delete Hello World!' });
+});
+
+//car路径  中间件
+router.get('/car', function(req, res, next) {
+  // v0.1版  前面是模板文件名  后面是传入到页面中的对象
+  console.log('这里是中间件');
+  // 继续下一步，下一个中间件
+  next();
+}, function(req, res, next) {
+  console.log('这里是返回');
+  // 向页面发送数据
+  res.send('这里是返回')
+});
+
 module.exports = router;
