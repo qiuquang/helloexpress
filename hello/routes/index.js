dfa811d1f6e547dc80df92ba234cec80ac547513
qiuquan <qiuquan@qdcares.cn>
2020-09-24 16:46:44 +0800

    加注释

--- hello/routes/index.js
+++ hello/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+//首页路由
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
